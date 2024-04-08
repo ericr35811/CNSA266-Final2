af8ec718d25293083ba0d6bccb89e01024bc5a1d
Eric R <ericr35811@gmail.com>
2024-04-08 10:50:43 -0400

    flask

---
+++ startapp.sh
@@ -0,0 +1,6 @@
+#export PYTHONHOME=/usr/lib/python2.7
+#export PYTHONPATH=/usr/lib/python2.7
+export PYTHONHOME=~/.local/lib/python2.7:~/.local/lib/python2.7/site-packages
+export PYTHONPATH=/usr/lib/python2.7:/usr/lib/python2.7/site-packages
+#export PYTHONPATH=/usr/lib/python2.7
+python2 $@
