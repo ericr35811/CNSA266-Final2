89a3e1b5b02161c5136a33cea31855121cbcb276
Eric R <ericr35811@gmail.com>
2024-04-09 17:36:04 -0400

    add SSH startup script for Linux

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 if [ $1- == '-d-' ]
 then
 	# debug output with -d switch
-	conda run -n py2test --live-stream gunicorn --pid gunicorn.pid --config gunicorn.conf.py app:app
+	conda run -n py2test --live-stream gunicorn --pid $PIDFILE --config gunicorn.conf.py app:app
 else
-	conda run -n py2test gunicorn --pid gunicorn.pid --config gunicorn.conf.py app:app
+	conda run -n py2test gunicorn --pid $PIDFILE --config gunicorn.conf.py app:app
 fi
 
 
