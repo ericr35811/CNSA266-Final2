caca20ab54ca386d75fb8ef9750abf2e8c72fee5
Eric Reinhardt <ewreinhardt221@stevenscollege.edu>
2024-04-09 15:45:49 -0400

    wip

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, session
 from psutil import cpu_percent
 from datetime import datetime
+from socket import gethostname
+
+from os import devnull
 
 app = Flask(__name__)
 
@@ -21,5 +24,11 @@ def index():
 
 if __name__ == '__main__':
 	#app.run(debug=True)
-	app.run(host="100.103.188.37")
-#	app.run()
+	# get ipv4 address of tailscale
+
+	if gethostname() == 'raspberrypi':
+		ip = '100.103.188.37'
+	else:
+		ip = '127.0.0.1'
+
+	app.run(host=ip)
