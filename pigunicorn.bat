8ec420a093bca3562dde5971fed5bfd967f3ab4c
Eric R <ericr35811@gmail.com>
2024-04-10 08:35:52 -0400

    windows ssh start script

--- pigunicorn.bat
+++ pigunicorn.bat
@@ -1,9 +1,20 @@
 @echo off
 
+set proj_dir=/home/eric/CNSA266-Final
+set conda_path=/home/eric/miniforge3/condabin
+set branch=webpage-4-8
+set pidfile=gunicorn.pid
+
 REM pull repository and start gunicorn via ssh
-start /wait ssh eric@raspberrypi "cd /home/eric/CNSA266-Final && git pull origin webpage-4-8 && PATH=$PATH:/home/eric/miniforge3/condabin ~/CNSA266-Final/start.sh -d"
 
-start /wait ssh eric@raspberrypi "cd /home/eric/CNSA266-Final && cat gunicorn.pid | xargs kill"
+set c=cd %proj_dir%
+set c=%c% ^^^&^^^& git pull origin %branch%
+set c=%c% ^&^& PIDFILE=%pidfile% PATH=$PATH:%conda_path% %proj_dir%/start.sh -d
 
+start /wait ssh eric@raspberrypi "%c%"
 
-REM kill gunicorn after previous command finishes
+set c=cd %proj_dir%
+set c=%c% ^^^&^^^& cat %pidfile% ^^^| xargs kill
+set c=%c% ^&^& rm %pidfile%
+
+start /wait ssh eric@raspberrypi "%c%"
